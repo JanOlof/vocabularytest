797de965e29d05615d2708cf78fdbdbea57b6183
Jan-olof Karlsson (HV) <imjk@hv.se>
2020-07-23 15:29:45 +0200

    Gör om fel klart

--- components/testpage.js
+++ components/testpage.js
@@ -6,7 +6,7 @@ import styles from '../styles';
 
 
 function TestPage({ navigation }) {
-    const [currentId, setCurrentId] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(0);
     const [inputOk, setInputOk] = useState(false);
     const [hasShownCorrectAnswer, setHasShownCorrectAnswer] = useState(false);
 
@@ -26,7 +26,7 @@ function TestPage({ navigation }) {
         var localDataComplex = '[ { "Id": 1, "Word1": [ { "Id": 1, "Text": "Hej", "Language": "Svenska", "LanguageId": 1 } ], "Word2": [ { "Id": 2, "Text": "Salut", "Language": "Franska", "LanguageId": 2 },  { "Id": 10, "Text": "Bonjour", "Language": "Franska", "LanguageId": 2 } ] }, { "Id": 2, "Word1": [ { "Id": 6, "Text": "Köpa", "Language": "Svenska", "LanguageId": 1 }, { "Id": 13, "Text": "Handla", "Language": "Svenska", "LanguageId": 1 } ], "Word2": [ { "Id": 7, "Text": "Acheter", "Language": "Franska", "LanguageId": 2 } ] }, { "Id": 3, "Word1": [ { "Id": 14, "Text": "Hus", "Language": "Svenska", "LanguageId": 1 } ], "Word2": [ { "Id": 15, "Text": "Maison", "Language": "Franska", "LanguageId": 2 } ] }, { "Id": 6, "Word1": [ { "Id": 4, "Text": "Bil", "Language": "Svenska", "LanguageId": 1 } ], "Word2": [ { "Id": 1020, "Text": "Voiture", "Language": "Franska", "LanguageId": 2 } ] }, { "Id": 7, "Word1": [ { "Id": 1021, "Text": "Åka", "Language": "Svenska", "LanguageId": 1 }, { "Id": 1023, "Text": "köra", "Language": "Svenska", "LanguageId": 1 } ], "Word2": [ { "Id": 1022, "Text": "Aller", "Language": "Franska", "LanguageId": 2 } ] }, { "Id": 8, "Word1": [ { "Id": 1025, "Text": "Sol", "Language": "Svenska", "LanguageId": 1 } ], "Word2": [ { "Id": 1024, "Text": "Soleil", "Language": "Franska", "LanguageId": 2 } ] }, { "Id": 9, "Word1": [ { "Id": 1028, "Text": "Regn", "Language": "Svenska", "LanguageId": 1 } ], "Word2": [ { "Id": 1029, "Text": "Pleut", "Language": "Franska", "LanguageId": 2 } ] }, { "Id": 10, "Word1": [ { "Id": 1033, "Text": "Dator", "Language": "Svenska", "LanguageId": 1 } ], "Word2": [ { "Id": 1034, "Text": "Ordinateur", "Language": "Franska", "LanguageId": 2 } ] }, { "Id": 12, "Word1": [ { "Id": 1037, "Text": "instängd", "Language": "Svenska", "LanguageId": 1 } ], "Word2": [ { "Id": 1038, "Text": "confine", "Language": "Franska", "LanguageId": 2 } ] }, { "Id": 13, "Word1": [ { "Id": 1039, "Text": "Strand", "Language": "Svenska", "LanguageId": 1 } ], "Word2": [ { "Id": 1040, "Text": "Plage", "Language": "Franska", "LanguageId": 2 } ] }]';
         var data = JSON.parse(localDataComplex);
         setWordPairs(data); 
-        initWordPairResults();
+        initWordPairResults(data.length);
 
 
     // Om isLoading är sant hämtas data en gång från mitt API som körs lokalt.
@@ -45,10 +45,12 @@ function TestPage({ navigation }) {
   }, [])
   )
 
-  function initWordPairResults(){
-    for (var i = 0; i < wordPairs.length; i++) {
-        wordPairsResult.push(false);
-      }
+  function initWordPairResults(length){
+    var arr =  [length]; 
+    for (var i = 0; i < length; i++) {
+        arr[i] = false;
+    }
+    setWordPairsResult (arr);
   }
 
     function changeLanguage(){
@@ -56,54 +58,72 @@ function TestPage({ navigation }) {
             setWord1ToWord2(false);
         else
             setWord1ToWord2(true);
-        
     }
 
     function nextLetter(){
         setHasShownCorrectAnswer(true);
         var length = input.length;
         if(word1ToWord2)
-            setInput(wordPairs[currentId].Word2[0].Text.substring(0, length + 1));
+            setInput(wordPairs[currentIndex].Word2[0].Text.substring(0, length + 1));
         else
-            setInput(wordPairs[currentId].Word1[0].Text.substring(0, length + 1));
+            setInput(wordPairs[currentIndex].Word1[0].Text.substring(0, length + 1));
     }
 
     function nextWord(){
-        if(currentId < wordPairs.length - 1) { 
-            setCurrentId(currentId + 1);
+        if(currentIndex < wordPairs.length - 1) { 
+            setCurrentIndex(getNextIndex());
+            setHasShownCorrectAnswer(false);
             setInput('');
             setInputOk(false);
         }
     }
-    //Två lägen: första gången 
+   
     function getNextIndex(){
-        for (var i = 0; i < wordPairs.length; i++) {
-            if(wordPairsResult[i] == true)
-                return i;
-          }
-
-    }
-    function previousWord(){
-        if(currentId > 0) { 
-            setCurrentId(currentId - 1);
-            setInput('');
-            setInputOk(false);
+        if(currentIndex < wordPairs.length - 1) {
+            for (var i = currentIndex + 1; i < wordPairs.length; i++) {
+                if(wordPairsResult[i] == false){
+                    return i;
+                }
+            }
         }
+        alert ('Du har kommit till slutet')
+        return currentIndex;
     }
+
+    function restartErrors(){
+        for (var i = 0; i < wordPairs.length; i++) {
+            if(wordPairsResult[i] == false){
+                setCurrentIndex(i);
+                setInput('');
+                setInputOk(false);
+                setHasShownCorrectAnswer(false);
+                return;
+            }
+        }
+        alert ('Du har klarat alla :)')
+    }
+
+    function restart(){
+        initWordPairResults( wordPairs.length);
+        setCurrentIndex(0);
+        setInput('');
+        setInputOk(false);
+        setHasShownCorrectAnswer(false);
+    }
+
     function showAnswer(){
         setHasShownCorrectAnswer(true);
         if(wordPairs.length > 0){
             var wordText = "";
             if(word1ToWord2){
-            wordPairs[currentId].Word2.map((word) =>(  
+            wordPairs[currentIndex].Word2.map((word) =>(  
                  wordText += word.Text + " "
             )); 
             }else{
-            wordPairs[currentId].Word1.map((word) =>(  
+            wordPairs[currentIndex].Word1.map((word) =>(  
                 wordText += word.Text + " "
                 )); 
             }
-
             setInput(wordText);
         }
     }
@@ -111,12 +131,12 @@ function TestPage({ navigation }) {
     function getWordSummary(){
         var wordText = "";
         if(word1ToWord2){
-        wordPairs[currentId].Word1.map((word) =>(  
+        wordPairs[currentIndex].Word1.map((word) =>(  
              wordText += word.Text + " "
         )); 
         }
         else{
-            wordPairs[currentId].Word2.map((word) =>(  
+            wordPairs[currentIndex].Word2.map((word) =>(  
                 wordText += word.Text + " "
            )); 
         }
@@ -126,12 +146,12 @@ function TestPage({ navigation }) {
     function checkAnswer(){
         if(wordPairs.length > 0){
             if(word1ToWord2){
-            wordPairs[currentId].Word2.map((word) =>(  
+            wordPairs[currentIndex].Word2.map((word) =>(  
                 verifyWord(word.Text)
                 )); 
             }
             else{
-                wordPairs[currentId].Word1.map((word) =>(  
+                wordPairs[currentIndex].Word1.map((word) =>(  
                     verifyWord(word.Text)
                 )); 
             }
@@ -139,35 +159,30 @@ function TestPage({ navigation }) {
     }
 
     function verifyWord(wordToCheck){
-    
         if(input.toLowerCase().trim( ) == wordToCheck.toLowerCase()){
             setInputOk(true);
             if(hasShownCorrectAnswer == false)
-                wordPairsResult[currentId] = true;
+                wordPairsResult[currentIndex] = true;
         }
-
     }
 
     useEffect(() => {
         setInputOk(false);
         checkAnswer(); 
     })
- 
-  
+   
     return(
-        
-           
         <View  style = { styles.container } >   
               { wordPairs.length > 0 ? (<View>
                 <View style={styles.flexDetailContainer} >
-                          <Text style = {styles.textGeneral}> {currentId + 1}/{wordPairs.length}</Text> 
+                          <Text style = {styles.textGeneral}> {currentIndex + 1}/{wordPairs.length}</Text> 
                           <View style={{padding:15} } /> 
                 <Button title ='Växla språk' onPress = {() => changeLanguage()} />   
                           </View>
                           <View style={{padding:5} } />
                           <Text style = {styles.textStyleHeader}> {getWordSummary()}</Text> 
                           </View>)
-                          : <Text> {currentId} Hämtar ordlista...</Text>}
+                          : <Text> {currentIndex} Hämtar ordlista...</Text>}
 
            <TextInput 
             style={[inputOk ? styles.topInputOk : styles.topInput]}
@@ -177,19 +192,17 @@ function TestPage({ navigation }) {
                 setInput(value);
                 checkAnswer()
             }}/>
-            
-            <Button title ='Visa svar' onPress = {() => showAnswer()} />  
-            <View style={{padding:5} } />
             <View style={styles.flexDetailContainer} >
-                
-            <Button title ='Nästa bokstav' onPress = {() => nextLetter()} />  
-            <View style={{padding:15} } /> 
-                <Button title ='<' onPress = {() => previousWord()} />   
-                <View style={{padding:15} } /> 
-                <Button title ='>' onPress = {() => nextWord()} />   
+                <Button title ='Bokstavshjälp' onPress = {() => nextLetter()} /> 
+                <View style={{padding:15} } />
+                <Button title ='Nästa ord >>' onPress = {() => nextWord()} /> 
             </View>  
-            
-
+            <View style={{padding:5} } />
+            <Button title ='Svar' onPress = {() => showAnswer()} />  
+            <View style={{padding:5} } />
+            <Button title ='Gör om felen' onPress = {() => restartErrors()} />   
+            <View style={{padding:5} } />
+            <Button title ='Gör om alla' onPress = {() => restart()} />   
 
             {/*Laddar tills innehållet kommer fram*/}
             {isLoading == true && <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
