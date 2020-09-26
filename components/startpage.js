129d76e3d2b53a5762ec9adf67eadc7a7b1d1d21
Jan-olof Karlsson (HV) <imjk@hv.se>
2020-09-26 09:16:44 +0200

    Lite justeringar i startpage

--- components/startpage.js
+++ components/startpage.js
@@ -12,8 +12,7 @@ export default function StartPage({ route, navigation }) {
     React.useCallback(() => {
       // Om isLoading är sant hämtas data en gång från mitt API som körs lokalt.
       if (isLoading == true) {
-        fetch('http://193.10.202.70/WordLearn/api/wordgroups')
-          //fetch('http://192.168.10.134:50000/api/wordgroups')
+        fetch('http://193.10.202.70/WordLearn/api/wordgroups') //fetch('http://192.168.10.134:50000/api/wordgroups')
           .then(response => response.json())
           .then(data => {
             console.log(data)
@@ -21,7 +20,7 @@ export default function StartPage({ route, navigation }) {
             setWordgroups(data)
           })
           .catch(error => {
-            console.log("Nu är det fel");
+            console.log("Fel vid hämtning av data från api");
             console.error(error);
           });
       }
@@ -30,45 +29,48 @@ export default function StartPage({ route, navigation }) {
 
   return (
     <View style={styles.background}>
-
       <View style={styles.container}>
-
         <SafeAreaView style={styles.container}>
-          {isLoading == true && <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Vänta medan data laddas ...</Text>
-            <ActivityIndicator size="large" color="#aa0707" />
-          </View>}
-          <View style={styles.rowContainer} >
-            <View style={styles.buttonContainer} >
-              <Text style={styles.textGeneral}>Ordprov</Text>
+          {isLoading == true ?
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+              <Text>Vänta medan data laddas ...</Text>
+              <View style={{ padding: 5 }} />
+              <ActivityIndicator size="large" color="#aa0707" />
             </View>
-            <View  >
-              <Text style={styles.textGeneral}>Ordlista</Text>
-            </View>
-          </View>
-
-          <FlatList
-            data={wordgroups}
-            keyExtractor={item => item.Id.toString()}
-            renderItem={({ item }) =>
-              /* Skapar ett nytt objekt med property Id i stället för att skicka hela item -  förstår inte varför det krävs :( */
+            :
+            <View>
               <View style={styles.rowContainer} >
-                <TouchableOpacity style={[styles.buttonBase, styles.buttonStyleNormal, styles.button80]} onPress={() => navigation.navigate('Test', { Id: item.Id })}>
-                  <Text>{item.Description}</Text>
-                </TouchableOpacity>
-                <View style={{ padding: 5 }} />
-                <TouchableOpacity style={[styles.buttonBase, styles.buttonStyleNormal, styles.button10]} onPress={() => navigation.navigate('AllWords', { Id: item.Id })}>
-                  <Text><AntDesign name="table" size={16} color="black" /></Text>
-                </TouchableOpacity>
+                <View style={styles.buttonContainer} >
+                  <Text style={styles.textGeneral}>Ordprov</Text>
+                </View>
+                <View  >
+                  <Text style={styles.textGeneral}>Ordlista</Text>
+                </View>
               </View>
-            }
-          />
+
+              <FlatList
+                data={wordgroups}
+                keyExtractor={item => item.Id.toString()}
+                renderItem={({ item }) =>
+                  /* Skapar ett nytt objekt med property Id i stället för att skicka hela item -  förstår inte varför det krävs :( */
+                  <View style={styles.rowContainer} >
+                    <TouchableOpacity style={[styles.buttonBase, styles.buttonStyleNormal, styles.button80]} onPress={() => navigation.navigate('Test', { Id: item.Id })}>
+                      <Text>{item.Description}</Text>
+                    </TouchableOpacity>
+                    <View style={{ padding: 5 }} />
+                    <TouchableOpacity style={[styles.buttonBase, styles.buttonStyleNormal, styles.button10]} onPress={() => navigation.navigate('AllWords', { Id: item.Id })}>
+                      <Text><AntDesign name="table" size={16} color="black" /></Text>
+                    </TouchableOpacity>
+                  </View>
+                }
+              />
+            </View>
+          }
         </SafeAreaView>
         <View style={styles.rowContainer}>
           <Image source={require('../assets/books.png')} style={{ width: 58, height: 50 }} />
           <Text>Version 1.0.3</Text>
         </View>
-      
       </View>
     </View>
   );
